1a945b95156667cab1e1c4d07c0443671a7083aa
Casey Cooley <satchmobgw@gmail.com>
2024-01-27 18:33:41 +0900

    Cutscene update made, working smoothly
    Will need to change the update return for game state change

--- cutscene_lib.lua
+++ cutscene_lib.lua
@@ -1,16 +1,8 @@
 
--- creates the text box, message, draws the speaker
-function cutscene(s)
-    message_box_draw(s)
-    if s.messages[1].speaker == "king" then
-     draw_king()
-    elseif s.messages[1].speaker == "mc" then
-    draw_mc()
-    end
-end
    
 -- draw and pop up the message box
-function message_box_draw(s)
+function cutscene(s)
+    cls()
    if s.new_scene then
     rectfill(2,2,mid(2,s.message_box_frame*6,126),2,13)
     rect(2,2,mid(2,s.message_box_frame*6,126),2,6)
@@ -19,20 +11,30 @@ function message_box_draw(s)
         rect(2,2,126,mid(2,(s.message_box_frame-21)*3,64),6)
     end
    else
+    cls()
     rectfill(2,2,126,64,13)
     rect(2,2,126,64,6)
-    local new_string = sub(s.messages[1].message,1,mid(1,(s.message_box_frame)*0.5,#s.messages[1].message))
+    local new_string = sub(s.messages[dialog_i].message,1,mid(1,(s.message_box_frame)*0.8,#s.messages[dialog_i].message))
     dshad(new_string,5,5,10)
+   end
+   if s.messages[dialog_i].speaker == "king" then
+    draw_king()
+   elseif s.messages[dialog_i].speaker == "mc" then
+    draw_mc()
    end
 end
 
 function intro_init()
+    dialog_i = 1 -- each button press progresses to the next dialog
     return {
         messages = {
-                {message = "hello new jester. \ni have brought you from the \nfuture to entertain me.\n\nnow juggle!\njuggle like your life \ndepends on it!", speaker = "king"}
+                {message = "hello new jester. \ni have brought you from the \nfuture to entertain me.\n\nnow juggle!\njuggle like your life \ndepends on it!", speaker = "king"},
+                {message = "now i continue talking \nto check if this works", speaker = "king"},
+                {message = "sounds good", speaker = "mc"},
+                {message = "yep", speaker ="king"}
             },
             message_box_frame = 0,
-            new_scene = true
+            new_scene = true,
     }
 end
 
@@ -42,8 +44,14 @@ function cutscene_update(s)
         s.new_scene = false
         s.message_box_frame = 0
     end
-    if btn(4) or btn(5) then
-        print("yay")
+    if s.new_scene == false and btnp(4) or btnp(5) then
+        cls()
+        if dialog_i < #s.messages then
+            s.message_box_frame = 0
+            dialog_i+=1
+        else
+            -- return change state to game mode
+        end
     end
 end
 
@@ -59,8 +67,8 @@ end
 function draw_mc()
 -- will need to change sprites in final
  spr(1,2,36,4,4)
- spr(69,68,68,8,4)
 end
+
 -- drop shadow
 function dshad (str, x, y)
  print (str, x+1, y+1, 1)
